a951ab22fead5d4c2511a6eb74bbcef69fa6de2d
Sven Kaack <sven.kaack@gmx.de>
2025-04-24 10:05:36 +0200

    finalized journal MVP

--- src/journal.js
+++ src/journal.js
@@ -36,19 +36,40 @@ const mockPokemon = [
         name: "Pikachu",
         id: 25,
         image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
-        stats: { hp: 35, attack: 55, defense: 40, speed: 90 },
+        stats: {
+            hp: 35,
+            attack: 55,
+            defense: 40,
+            "special Attack": 50,
+            "special Defense": 40,
+            speed: 90,
+        },
     },
     {
         name: "Bulbasaur",
         id: 1,
         image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-        stats: { hp: 45, attack: 49, defense: 49, speed: 45 },
+        stats: {
+            hp: 45,
+            attack: 49,
+            defense: 49,
+            "special Attack": 65,
+            "special Defense": 65,
+            speed: 45,
+        },
     },
     {
         name: "Charmander",
         id: 4,
         image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
-        stats: { hp: 39, attack: 52, defense: 43, speed: 65 },
+        stats: {
+            hp: 39,
+            attack: 52,
+            defense: 43,
+            "special Attack": 60,
+            "special Defense": 50,
+            speed: 65,
+        },
     },
 ];
 
@@ -107,8 +128,8 @@ const displayFavPokemon = () => {
         const item = document.createElement("div");
         item.classList.add(
             "mt-10",
-            "bg-blue-200",
-            "border",
+            "bg-white",
+            "shadow-md",
             "rounded",
             "mx-auto",
             "w-70",
