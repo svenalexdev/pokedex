161b9f7c08224fe429d6cc284f27ecc0d6b75bed
Sven Kaack <sven.kaack@gmx.de>
2025-05-05 13:52:32 +0200

    Remove .parcel-cache from repo

--- Parcel/src/modules/storage.js
+++ Parcel/src/modules/storage.js
@@ -1,11 +1,8 @@
 // Storage for journal
 import { mockPokemon } from "./network";
 
-// Unclear: why is it still working if we change e.g. "favorites" to ""
-const favPokemonKey = "favorites";
-
 const getFavPokemon = () => {
-    const saved = JSON.parse(localStorage.getItem(favPokemonKey));
+    const saved = JSON.parse(localStorage.getItem("favorites"));
     // console.log(saved);
     return saved && saved.length ? saved : mockPokemon;
 };
